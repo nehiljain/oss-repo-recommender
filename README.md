5097ee37c18af93c731507e53741bdcb4c0d8cac
nehil jain <2761999+nehiljain@users.noreply.github.com>
2024-01-24 15:22:35 -0500

    Feat: Prompt for querying

--- README.md
+++ README.md
@@ -7,11 +7,12 @@
 graph TD
     A[Github Repos] -->|Markdown Chunking| B(Chunks)
     B --> C[AI Embedding Model]
-    C -->|Embeddings| D[Atlas Vector Search]
-    E[Question] --> F[Same Embedding Model]
+    C -->|Embeddings| D[Atlas Vector DB]
+    E((Question)) --> F[Same Embedding Model]
     F -->|Query Embeddings| D
     D --> |Semantic Similarity Search| H[Question + Context Documents]
     E -->  H
     H --> I[LLM]
-    I --> J[Answer]
+    I --> J((Answer))
+
 ```
---
+++ prompts.py
@@ -0,0 +1,22 @@
+select_perfet_repo = """
+You are a github searching agent who can look at different repos and recommend the right starter template for the >>>user_query<<<. You can use {{{ist of repository descriptions}}} and attached zip files to find the perfect match.
+
+The attached files have documentation in readme of the repo you can use to pull more information about example code, technology components etc..
+
+The goal is to fiind the repo that is the closest match for all the technology components required to build the solution.
+
+
+List of repository descriptions:
+{repo_description}
+
+
+User Query:
+{user_query}
+
+
+The output should be json with keys
+repo-name: The name of the repository
+link: Repo link I can clone for my project
+reason: Explanation of what the example is which will be helpful
+Dont give any other information and just the structure json ouptut."""
+"""
