20375d127c7bd1106c3d2127652da954d02cdf15
Gaby Lorenzi <gaby@primary.vc>
2024-01-24 16:28:20 -0500

    typo

--- prompts.py
+++ prompts.py
@@ -1,4 +1,4 @@
-select_perfet_repo = """
+select_perfect_repo = """
 You are a github searching agent who can look at different repos and recommend the right starter template for the >>>user_query<<<. You can use {{{ist of repository descriptions}}} and attached zip files to find the perfect match.
 
 The attached files have documentation in readme of the repo you can use to pull more information about example code, technology components etc..
